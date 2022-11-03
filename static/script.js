f2ec70003f2ce40ccd8125d3be274d0392efe981
cblatt <cblatt@uwo.ca>
2022-11-03 01:19:35 -0400

    search by track, artist, and album name

--- static/script.js
+++ static/script.js
@@ -1,27 +1,81 @@
-// running the searchTrack() function when you click on the search for track button
-// this will be implementing in searching by track name
-document.getElementById('searchTrackBtn').addEventListener('click', searchTrack);
 
+// stores the unordered list of search results
+var searchList = document.getElementById('searchList');
+
+// clear search results when the button is clicked
+function clearSearchResults(){
+    while(searchList.firstChild){
+        searchList.removeChild(searchList.firstChild);
+    }
+}
+document.getElementById('clearSearchResults').addEventListener('click', clearSearchResults); // clearing search results when clear button is pressed
+
+// search by track name
 function searchTrack(){
     var trackName = document.getElementById('searchTrackText').value;
-    var searchList = document.getElementById('searchList');
-    
+
     fetch('/api/trackNames/' + trackName)
     .then(res => res.json()
     .then(data => {
 
-        var header = document.createTextNode('Tracks:');
-        searchList.appendChild(header);
+        searchList.appendChild(document.createTextNode('Tracks:'));
         searchList.appendChild(document.createElement('br'));
-        
-        var tracksNode = document.createTextNode(data);
-        searchList.appendChild(tracksNode);
-       
+        searchList.appendChild(document.createTextNode(data));
+
     }));
-
-
-
-
-
 }
+document.getElementById('searchTrackBtn').addEventListener('click', clearSearchResults); // clearing search results when button is pressed
+document.getElementById('searchTrackBtn').addEventListener('click', searchTrack); // searching by track name
+
+// search by artist name
+function searchArtist(){
+    var artistName = document.getElementById('searchArtistText').value;
+
+    fetch('/api/artistNames/' + artistName)
+    .then(res => res.json()
+    .then(data => {
+
+        searchList.appendChild(document.createTextNode('Artists:'));
+        searchList.appendChild(document.createElement('br'));
+        searchList.appendChild(document.createTextNode(data))
+
+    }));
+}
+document.getElementById('searchArtistBtn').addEventListener('click', clearSearchResults); // clearing search results when button is pressed
+document.getElementById('searchArtistBtn').addEventListener('click', searchArtist); // searching by artist name
+
+// search by album name
+function searchAlbum(){
+    var albumName = document.getElementById('searchAlbumText').value;
+
+    fetch('/api/albumNames/' + albumName)
+    .then(res => res.json()
+    .then(data => {
+
+        searchList.appendChild(document.createTextNode('Albums:'));
+        searchList.appendChild(document.createElement('br'));
+        searchList.appendChild(document.createTextNode(data))
+
+    }));
+}
+document.getElementById('searchAlbumBtn').addEventListener('click', clearSearchResults); // clearing search results when button is pressed
+document.getElementById('searchAlbumBtn').addEventListener('click', searchAlbum); // searching by album name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
